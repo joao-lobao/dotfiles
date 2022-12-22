2d9d217e42686f57040d19521ba21662fc732983
joao <joao_loba@sapo.pt>
2022-12-22 20:30:09 -0100

    refactors backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -18,31 +18,22 @@ PINK="#eb6ce8"
 BLUE="#007cba"
 
 declare -A backup_dirs=(
-	[zsh]=$DOTFILES
-	[newsboat]=$DOTFILES
-	[scripts]=$DOTFILES
-	[tmux_conf]=$TMUX_HOME_BACKUP
-	[tmuxinator_sessions]=$TMUX_HOME_BACKUP
-	[tmuxinator_autocompletion]=$TMUX_HOME_BACKUP
-	[tmux_plugins]=$TMUX_PLUGINS_BACKUP
+	[$ZSH_SOURCE]=$DOTFILES
+	[$NEWSBOAT_SOURCE]=$DOTFILES
+	[$SCRIPTS_SOURCE]=$DOTFILES
+	[$TMUX_CONF_SOURCE]=$TMUX_HOME_BACKUP
+	[$TMUXINATOR_SESSIONS_SOURCE]=$TMUX_HOME_BACKUP
+	[$TMUXINATOR_AUTOCOMPLETION_SOURCE]=$TMUX_HOME_BACKUP
+	[$TMUX_PLUGINS_SOURCE]=$TMUX_PLUGINS_BACKUP
 )
 declare -A sources_dirs=(
-	[zsh]=$ZSH_SOURCE
-	[newsboat]=$NEWSBOAT_SOURCE
-	[scripts]=$SCRIPTS_SOURCE
-	[tmux_conf]=$TMUX_CONF_SOURCE
-	[tmuxinator_sessions]=$TMUXINATOR_SESSIONS_SOURCE
-	[tmuxinator_autocompletion]=$TMUXINATOR_AUTOCOMPLETION_SOURCE
-	[tmux_plugins]=$TMUX_PLUGINS_SOURCE
-)
-declare -a dirs=(
-	"zsh"
-	"newsboat"
-	"scripts"
-	"tmux_conf"
-	"tmuxinator_sessions"
-	"tmuxinator_autocompletion"
-	"tmux_plugins"
+	[$ZSH_SOURCE]=$ZSH_SOURCE
+	[$NEWSBOAT_SOURCE]=$NEWSBOAT_SOURCE
+	[$SCRIPTS_SOURCE]=$SCRIPTS_SOURCE
+	[$TMUX_CONF_SOURCE]=$TMUX_CONF_SOURCE
+	[$TMUXINATOR_SESSIONS_SOURCE]=$TMUXINATOR_SESSIONS_SOURCE
+	[$TMUXINATOR_AUTOCOMPLETION_SOURCE]=$TMUXINATOR_AUTOCOMPLETION_SOURCE
+	[$TMUX_PLUGINS_SOURCE]=$TMUX_PLUGINS_SOURCE
 )
 
 function color_text() {
@@ -51,7 +42,7 @@ function color_text() {
 	gum style --foreground "$color" "$text"
 }
 
-function copy_file_or_directory() {
+function backup_file_or_directory() {
 	if [ -d "$sources_dirs[$1]" ]; then
 		cp -rv $sources_dirs[$1] $backup_dirs[$1]
 	else
@@ -67,17 +58,26 @@ function backup() {
 		--margin "1" \
 		--padding "1" \
 		"Welcome to DOTFILES BACKUP tool"
-	directories_to_backup=$(gum choose --no-limit $dirs)
+	directories_to_backup=$(gum choose --no-limit $sources_dirs)
+	# transform directories_to_backup into an array
 	dirs_transformed=($(echo $directories_to_backup | tr ' ' "\n"))
+	# if directories_to_backup is empty, exit
 	if [ -z "$dirs_transformed" ]; then
 		color_text $RED "Exited without saving!"
 		return
 	fi
 
+	# iterate over directories_to_backup
 	for d in "${dirs_transformed[@]}"; do
 		color_text $BLUE "STARTING $d BACKUP"
-		copy_file_or_directory $d
-    color_text $PINK "FINISHED $d BACKUP"
-    echo -e
+		backup_file_or_directory $d
+		# if backup_file_or_directory fails and continue to next iteration
+		if [ $? -ne 0 ]; then
+			color_text $RED "FAILED TO BACKUP $d"
+			echo -e
+			continue
+		fi
+		color_text $PINK "FINISHED $d BACKUP"
+		echo -e
 	done
 }
