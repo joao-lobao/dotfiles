5ff6798f2019ca35c910a09700f5beef12542faa
Joao Lobao <joao_lobao@sapo.pt>
2023-04-03 22:01:09 +0000

    dotfiles backup path change

--- tmux/chtsh/cht.sh
+++ tmux/chtsh/cht.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-selected=`cat ~/Desktop/dotfiles/tmux/chtsh/.tmux-cht-languages | fzf`
+selected=`cat /media/home/SSD_BACKUP/dotfiles/tmux/chtsh/.tmux-cht-languages | fzf`
 # selected=`cat ~/Desktop/dotfiles/tmux/chtsh/.tmux-cht-languages ~/Desktop/dotfiles/tmux/chtsh/.tmux-cht-command | fzf`
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/Desktop/dotfiles/tmux/chtsh/.tmux-cht-languages; then
+if grep -qs "$selected" /media/home/SSD_BACKUP/dotfiles/tmux/chtsh/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     echo "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
     tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
