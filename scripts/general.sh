c71f29b081871d1fb3c4a6c0b74a38ca6b459c07
Joao Lobao <joao_lobao@sapo.pt>
2023-05-07 06:27:05 +0000

    add launch script

--- scripts/general.sh
+++ scripts/general.sh
@@ -26,3 +26,35 @@ function say() {
 	spd-say $2
 	color_text $1 $2
 }
+
+function spinner() {
+	title=$1
+	spinner=$2
+	color=$3
+	gum spin --spinner="$spinner" --title="$title" --title.foreground="$color" sleep 5 && color_text "#ff8888" "$title ✔"
+}
+
+function launch() {
+	phases=(
+		"Preparing for launch: This may take a few minutes ⏳"
+		"Preparing for launch: Fueling the rocket ⛽️"
+		"Preparing for launch: Preparing guidance systems 📡"
+		"Preparing for launch: Initiating launch sequence 🤖"
+		"Preparing for launch: Ignition 🔑"
+		"Launching: Lift-off! 🚀"
+		"Launching: Clearing the launch pad 💨"
+		"Ascending: Passing through the atmosphere 🌥"
+		"Pitching and rolling: Achieving desired trajectory 🕹"
+		"Staging: Separating from the booster 🔛"
+		"Orbital insertion: Using engine to achieve the correct velocity 🎚"
+		"Payload deployment: Deploying payload 🛰"
+	)
+
+	while true; do
+		for c in $phases; do
+			spinner "$c" "moon" "#ff0000"
+		done
+	done
+	while true; do
+	done
+}
