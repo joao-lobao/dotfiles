40cadb72c6fae11aa1ba18eed16410e2c6a68352
Joao Lobao <joao_lobao@sapo.pt>
2023-09-25 11:19:53 +0000

    change to display pomodoro times

--- scripts/pomodoro.sh
+++ scripts/pomodoro.sh
@@ -14,11 +14,11 @@ function pom() {
 	long_break="15m"
 	cycles=4
 	for ((j = 1; j <= $cycles; j++)); do
-		pom_chunk $PINK "working session $j" $work_time
+		pom_chunk $PINK "working session $j $work_time" $work_time
 		if ((j < $cycles)); then
-			pom_chunk $BLUE "short break" $short_break
+			pom_chunk $BLUE "short break $short_break" $short_break
 		fi
 	done
-	pom_chunk $BLUE "long break" $long_break
+	pom_chunk $BLUE "long break $long_break" $long_break
 	say $RED "You should now initiate another cycle or rest for today."
 }
